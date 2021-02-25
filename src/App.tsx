85411d82d1817642f98562b57ced71428549e0d4
jroberto <jrobertoonb@gmail.com>
2021-02-25 15:51:21 -0300

    chore: adicionando estrutura do projeto moveit com react e typescript ao versionamento, já com um componente em andamento

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+import {} from './components/ExperienceBar';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
