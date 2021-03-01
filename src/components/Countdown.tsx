253592ffbae659efde519e3de2a171bcf326c7da
JrzenonDev <jrobertoonb@gmail.com>
2021-03-01 15:34:08 -0300

    chore: iniciando o hook de count down, já chamando a função com onclick, defini o minuto e segundo já com o calculo arrendondando corretamente e com verificação de quando está ativo e time > 0, depois reduz o tempo em 1 segundo

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from 'react';
 import style from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+
+  // hook de count down
+  const [time, setTime] = useState(26 * 60);
+
+  // criando um estado
+  const [active, setActive] = useState(false);
+
+  // arrendo os minutos para baixo
+  const minutes = Math.floor(time / 60);
+
+  // calcula pegando o resto da divisão
+  const secunds = time % 60;
+
+  // monta os minutos e converte em string o valor
+  // padStart (verifica se existe 2 caracteres, caso não passa o 0 para a esquerda, ex: o vaor "5" retornaria 05)
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secundLeft, secundRight] = String(secunds).padStart(2, '0').split('');
+
+  function startCountDown() {
+    setActive(true);
+
+
+
+  }
+
+  // setando o use effect, inicia com uma function (no caso usei uma arrow function), depois o quando ele deve disparar a função, no caso toda vez que active mudar
+  useEffect(() => {
+    if(active && time > 0) {
+      setTimeout(() => {
+        setTime(time -1);
+      }, 1000);
+    }
+    console.log(active);
+  }, [active, time])
 
 
   return(
@@ -9,18 +44,22 @@ export function Countdown() {
         <div className={style.countdownContainer}>
 
           <div>
-            <span>2</span>
-            <span>5</span>
+            <span>{minuteLeft}</span>
+            <span>{minuteRight}</span>
           </div>
           <span>:</span>
           <div>
-            <span>8</span>
-            <span>8</span>
+            <span>{secundLeft}</span>
+            <span>{secundRight}</span>
           </div>
 
         </div>
 
-        <button type="button" className={style.countdownButton}>
+        <button
+          type="button"
+          className={style.countdownButton}
+          onClick={startCountDown}
+        >
           Iniciar um ciclo
         </button>
 
